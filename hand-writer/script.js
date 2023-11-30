4e24f3cff7b8b3aceb94e669f442d27ccafbba5a
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2023-11-30 14:15:13 -0300

    Improve hand-writer

--- hand-writer/script.js
+++ hand-writer/script.js
@@ -6,9 +6,13 @@ const tiles = document.getElementsByClassName('tile');
 
 for (const tile of tiles) {
 	tile.addEventListener('pointerdown', () => {
-		tile.style.backgroundColor = 'darkblue';
+		tile.style.backgroundColor = '#4000EB';
+		tile.style.border = 'solid 1px white';
 	});
     tile.addEventListener('pointerover', () => {
-        if (pointerdown) tile.style.backgroundColor = 'darkblue';
+        if (pointerdown) {
+			tile.style.backgroundColor = '#4000EB';
+			tile.style.border = 'solid 1px white';
+		}
     });
 }
