c127e9b66ccfb92db501ac4a76a850c8547e1059
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2024-02-08 07:47:21 -0300

    soulmate: create Soulmate class

--- soulmate/script.js
+++ soulmate/script.js
@@ -1,2 +1,12 @@
-const canvas = document.getElementsByTagName('canvas').item(0);
-const context = canvas ? canvas.getContext('2d') : console.log('Unable to get context due to canvas not found');
+class Soulmate {
+	#context;
+	
+	constructor() {
+		const canvas = document.getElementsByTagName('canvas').item(0);
+		if(!canvas) return console.log('Canvas not found');
+		if(!canvas.getContext) return console.log('Canvas not supported');
+		this.#context = canvas.getContext('2d');
+	}
+}
+
+const soulmate = new Soulmate();
