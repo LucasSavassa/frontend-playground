2b15960146189854bea50bea80178d460fda8257
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2024-02-12 15:56:05 -0300

    soulmate: implement animation

--- soulmate/script.js
+++ soulmate/script.js
@@ -1,5 +1,185 @@
+class Point {
+	#x;
+	#y;
+	
+	constructor (x, y) {
+		this.#x = x;
+		this.#y = y;
+	}
+	
+	get x() {
+		return this.#x;
+	}
+	
+	set x(value) {
+		this.#x = value;
+	}
+	
+	get y() {
+		return this.#y;
+	}
+	
+	set y(value) {
+		this.#y = value;
+	}
+}
+
+class Vector {
+	#x;
+	#y;
+	#magnitude;
+	
+	constructor(x, y, magnitude) {
+		this.#x = x;
+		this.#y = y;
+		this.#magnitude = magnitude;
+	}
+	
+	get x() {
+		return this.#x;
+	}
+	
+	get y() {
+		return this.#y;
+	}
+	
+	get magnitude() {
+		return this.#magnitude;
+	}
+}
+
+class She {
+	#context;
+	#vector;
+	#position;
+	#radius;
+	
+	constructor(context) {
+		this.#context = context;
+		this.#vector = new Vector(0,1,2);
+		this.#position = new Point(185, 200);
+		this.#radius = 15;
+	}
+	
+	draw() {
+		this.#context.beginPath();
+		this.#context.fillStyle = "rgb(247 202 201)";
+		this.#context.arc(this.#position.x, this.#position.y, this.#radius, 0, 2 * Math.PI, true);
+		this.#context.fill();
+	}
+	
+	updatePosition() {
+		this.#updateX();
+		this.#updateY();
+	}
+	
+	#updateX() {
+		if(this.#position.x > window.innerWidth - (this.#radius * 0.75))
+			this.#invertX();
+		
+		if(this.#position.x < 0 + (this.#radius * 0.75))
+			this.#invertX();
+		
+		const incrementX = this.#vector.x * this.#vector.magnitude;
+		this.#position.x += incrementX;
+	}
+	
+	#invertX() {
+		const x = this.#vector.x * -1;
+		const y = this.#vector.y;
+		const magnitude = this.#vector.magnitude;
+		
+		this.#vector = new Vector(x,y,magnitude);
+	}
+	
+	#updateY() {
+		if(this.#position.y > window.innerHeight - (this.#radius * 0.75))
+			this.#invertY();
+		
+		if(this.#position.y < 0 + (this.#radius * 0.75))
+			this.#invertY();
+		
+		const incrementY = this.#vector.y * this.#vector.magnitude;		
+		this.#position.y += incrementY;
+	}
+	
+	#invertY() {
+		const x = this.#vector.x;
+		const y = this.#vector.y * -1;
+		const magnitude = this.#vector.magnitude;
+		
+		this.#vector = new Vector(x,y,magnitude);
+	}
+}
+
+class He {
+	#context;
+	#vector;
+	#position;
+	#radius;
+	
+	constructor(context) {
+		this.#context = context;
+		this.#vector = new Vector(0,-1,2);
+		this.#position = new Point(215, 200);
+		this.#radius = 15;
+	}
+	
+	draw() {
+		this.#context.beginPath();
+		this.#context.fillStyle = "rgb(145 168 209)";
+		this.#context.arc(this.#position.x, this.#position.y, this.#radius, 0, 2 * Math.PI, true);
+		this.#context.fill();	
+	}
+	
+	updatePosition() {
+		this.#updateX();
+		this.#updateY();
+	}
+	
+	#updateX() {
+		if(this.#position.x > window.innerWidth - (this.#radius * 0.67))
+			this.#invertX();
+		
+		if(this.#position.x < 0 + (this.#radius * 0.67))
+			this.#invertX();
+		
+		const incrementX = this.#vector.x * this.#vector.magnitude;
+		this.#position.x += incrementX;
+	}
+	
+	#invertX() {
+		const x = this.#vector.x * -1;
+		const y = this.#vector.y;
+		const magnitude = this.#vector.magnitude;
+		
+		this.#vector = new Vector(x,y,magnitude);
+	}
+	
+	#updateY() {
+		if(this.#position.y > window.innerHeight - (this.#radius * 0.67))
+			this.#invertY();
+		
+		if(this.#position.y < 0 + (this.#radius * 0.67))
+			this.#invertY();
+		
+		const incrementY = this.#vector.y * this.#vector.magnitude;		
+		this.#position.y += incrementY;
+	}
+	
+	#invertY() {
+		const x = this.#vector.x;
+		const y = this.#vector.y * -1;
+		const magnitude = this.#vector.magnitude;
+		
+		this.#vector = new Vector(x,y,magnitude);
+	}
+}
+
 class Soulmate {
 	#context;
+	#she;
+	#he;
 	
 	constructor() {
 		const canvas = document.getElementsByTagName('canvas').item(0);
@@ -8,25 +188,20 @@ class Soulmate {
 		this.#context = canvas.getContext('2d');
 		this.#context.canvas.width = window.innerWidth;
 		this.#context.canvas.height = window.innerHeight;
+		this.#she = new She(this.#context);
+		this.#he = new He(this.#context);
 	}
 	
-	render() {
-		this.#she();
-		this.#he();
-	}
-	
-	#she() {
-		this.#context.beginPath();
-		this.#context.fillStyle = "rgb(247 202 201)";
-		this.#context.arc(185, 200, 15, 0, 2 * Math.PI, true);
-		this.#context.fill();
-	}
-	
-	#he() {
-		this.#context.beginPath();
-		this.#context.fillStyle = "rgb(145 168 209)";
-		this.#context.arc(215, 200, 15, 0, 2 * Math.PI, true);
-		this.#context.fill();	
+	draw() {
+		this.#context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+		
+		this.#she.updatePosition();
+		this.#she.draw();
+		
+		this.#he.updatePosition();
+		this.#he.draw();
+		
+		requestAnimationFrame(this.draw.bind(this));
 	}
 }
 
