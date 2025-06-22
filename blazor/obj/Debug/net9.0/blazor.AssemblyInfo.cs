827308d64c8e217caeec0fa2ffb7146cbe2f1aa8
Savassa, Lucas <Lucas.Savassa-ext2@ab-Inbev.com>
2025-06-22 11:42:02 -0300

    ./blazor: test interactive render mode and data binding

--- blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
+++ blazor/obj/Debug/net9.0/blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82133e7187dc0946f1476f342ae8b51f5a9baa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7046470e4320124f2027c7adffce639539ebddfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
